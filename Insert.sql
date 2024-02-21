96dc146daf1f4491768ded9d6ebe6a4e42342171
hoangviet24 <hoangvietdoican2004@gmail.com>
2024-02-21 23:16:44 +0700

    Origin data

--- Insert.sql
+++ Insert.sql
@@ -35,6 +35,56 @@ INSERT INTO [dbo].[Product]
 		   ,0
            ,1)
 GO
-
+INSERT INTO [dbo].[Product]
+           ([Name]
+           ,[ImageUrl]
+           ,[Detail]
+           ,[Price]
+           ,[isTrendingProduct]
+           ,[isTopSaleProduct]
+           ,[isTopPlayProduct])
+     VALUES
+           ('Warframe'
+           ,'https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-6/425798983_1844130872703804_973563999780001148_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=IK0lcWmMZq4AX_McADu&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfDLxgzmmMqQc3QqsDy0gnAgilHhip0PYSg2fIN2yOF9wg&oe=65DAD2B1'
+           ,N'Game RPG bắn súng hay nhất'
+           ,0
+           ,1
+		   ,0
+           ,1)
+GO
+INSERT INTO [dbo].[Product]
+           ([Name]
+           ,[ImageUrl]
+           ,[Detail]
+           ,[Price]
+           ,[isTrendingProduct]
+           ,[isTopSaleProduct]
+           ,[isTopPlayProduct])
+     VALUES
+           ('Diablo Immortal'
+           ,'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/426920263_1845376715912553_3470706109835194956_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=3635dc&_nc_ohc=orS1JQdELLkAX-Q3XYr&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfCq7t3ypU8wQ6IjTVlcqhEKKcO4_HNFT9MyzNR8X4_aLQ&oe=65DB61E1'
+           ,N'Chưa chơi game này nên chưa thể cảm nhận'
+           ,1000000
+           ,1
+		   ,1
+           ,1)
+GO
+INSERT INTO [dbo].[Product]
+           ([Name]
+           ,[ImageUrl]
+           ,[Detail]
+           ,[Price]
+           ,[isTrendingProduct]
+           ,[isTopSaleProduct]
+           ,[isTopPlayProduct])
+     VALUES
+           ('Diablo IV'
+           ,'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-6/426411694_1845376702579221_221871689530672809_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=3635dc&_nc_ohc=0t2a6QfPXcIAX_D3tRV&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfCnS8FYwKNFY6zAZw0akZqUsYTQllLmTJlXUvAwkMvgew&oe=65DA40AE'
+           ,N'Game hết hay'
+           ,1000000
+           ,1
+		   ,0
+           ,1)
+GO
 
 
